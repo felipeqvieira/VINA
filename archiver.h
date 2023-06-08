f1ac1d40e027b67b5047fe838e441fc2fce59524
Felipe Quaresma Vieira <felipeqvieira1@outlook.com>
2023-06-07 21:25:51 -0300

    Coloquei as funções no .h

--- archiver.h
+++ archiver.h
@@ -31,3 +31,31 @@ typedef struct {
     Archive archive;
 
 }Archiver;
+
+void inicializar_membro(Membro** membro, const char* nome);
+
+void inicializar_archive(Archive * archive);
+
+void preencher_dados_membro(Membro* membro, const char* nome_arquivo);
+
+void adicionar_membro(Archive* archive, const char* nome_membro, const char* nome_arquivo_destino);
+
+void substituir_membro(Membro* novo_membro, int indice_existente, Membro* membro_existente);
+
+int encontrar_nome (Archive* archive, const char* nome_membro);
+
+int adiciona_verificando_data(Archive* archive, const char* nome_membro, const char* arquivo_destino);
+
+void atualizar_diretorio(Archive* archive, FILE* arquivo);
+
+void escrever_arquivo_archive(Archive* archive);
+
+int mover_membro(Archive *archive, const char* membro_frente, const char* membro_atras);
+
+void extrair_membros(Archive *archive, const char* nome_membro);
+
+void listar_conteudo(const char* nome_arquivo);
+
+void remover_membro(const char* nome_arquivo, const char* nome_membro);
+
+void exibir_ajuda();
