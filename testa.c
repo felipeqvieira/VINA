f4ad856147bb7caffc9c6ff4160cf98e8519d470
Felipe Quaresma Vieira <felipeqvieira1@outlook.com>
2023-06-12 12:25:02 -0300

    Criei um documento para explicar cada função e um testa.c que precisa ser complementado.

---
+++ testa.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Defina as estruturas e as funções aqui
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        printf("É necessário fornecer o nome do arquivo como argumento.\n");
+        return 1;
+    }
+
+    char *nome_arquivo = argv[1];
+
+    // Outras variáveis necessárias
+    Archive archive;
+    int opcao;
+
+    // Opções do usuário
+    printf("Selecione uma opção:\n");
+    printf("1. Inicializar Archive\n");
+    printf("2. Adicionar Membro\n");
+    printf("3. Substituir Membro\n");
+    printf("4. Encontrar Nome\n");
+    printf("5. Adicionar Verificando Data\n");
+    printf("6. Atualizar Diretório\n");
+    printf("7. Escrever Arquivo Archive\n");
+    printf("8. Mover Membro\n");
+    printf("9. Extrair Membros\n");
+    printf("10. Listar Conteúdo\n");
+    printf("11. Remover Membro\n");
+    printf("12. Exibir Ajuda\n");
+    printf("0. Sair\n");
+    printf("Opção: ");
+    scanf("%d", &opcao);
+
+    switch (opcao) {
+        case 1:
+            // Chamar a função inicializar_archive
+            break;
+        case 2:
+            // Chamar a função adicionar_membro
+            break;
+        case 3:
+            // Chamar a função substituir_membro
+            break;
+        case 4:
+            // Chamar a função encontrar_nome
+            break;
+        case 5:
+            // Chamar a função adiciona_verificando_data
+            break;
+        case 6:
+            // Chamar a função atualizar_diretorio
+            break;
+        case 7:
+            // Chamar a função escrever_arquivo_archive
+            break;
+        case 8:
+            // Chamar a função mover_membro
+            break;
+        case 9:
+            // Chamar a função extrair_membros
+            break;
+        case 10:
+            // Chamar a função listar_conteudo
+            break;
+        case 11:
+            // Chamar a função remover_membro
+            break;
+        case 12:
+            // Chamar a função exibir_ajuda
+            break;
+        case 0:
+            // Encerrar o programa
+            break;
+        default:
+            printf("Opção inválida.\n");
+            break;
+    }
+
+    return 0;
+}
