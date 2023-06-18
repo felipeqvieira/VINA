b70a4d0001ce8d6f9f590619d640739471138dc5
Felipe Quaresma Vieira <felipeqvieira1@outlook.com>
2023-06-17 21:37:49 -0300

    Função extrair membro feita.

--- teste1.c
+++ teste1.c
@@ -470,87 +470,120 @@ void extrair_membros(Archiver* archiver, char** membros, int num_membros) {
 }
 
 int main(int argc, char *argv[]) {
-    // Verificar se foram fornecidos argumentos suficientes
-    if (argc < 3) {
-        printf("Uso: vina++ <opção> <archive> [membro1 membro2 ...]\n");
-        return 1;
-    }
+    int continuar = 1;
 
-    char *opcao = argv[1];
     char *archive = argv[2];
-    char **membros = argv + 3;  // Pode ser vazio, dependendo da opção
 
-    // Convertendo caminhos absolutos para caminhos relativos
-    char *diretorio_atual = getcwd(NULL, 0);  // Obtém o diretório atual
-    int tamanho_diretorio_atual = strlen(diretorio_atual);
-
-    for (int i = 0; membros[i] != NULL; i++) {
-        if (membros[i][0] != '/') {
-            // O membro não é um caminho absoluto, ignora a conversão
-            continue;
-        }
-
-        char *caminho_relativo = malloc(strlen(membros[i]) + 2 + tamanho_diretorio_atual);
-        if (caminho_relativo == NULL) {
-            printf("Erro ao alocar memória.\n");
+    Archiver *archiver = malloc(sizeof(Archiver));
+        if (archiver == NULL) {
+            printf("Erro ao alocar memória para o Archiver.\n");
             return 1;
         }
 
-        strcpy(caminho_relativo, ".");
-        strcat(caminho_relativo, membros[i]);
-
-        membros[i] = caminho_relativo;
-    }
-
-    free(diretorio_atual);
-
-    Archiver *archiver = malloc(sizeof(Archiver));
-    if (archiver == NULL) {
-        printf("Erro ao alocar memoria para o Archiver.\n");
-        return 1;
-    }
-
     ArchiveData *archiveData = inicializar_archive(archive);
-    if (archiveData == NULL) {
-        printf("Erro ao inicializar o ArchiveData.\n");
-        free(archiver);
-        return 1;
-    }
+        if (archiveData == NULL) {
+            printf("Erro ao inicializar o ArchiveData.\n");
+            free(archiver);
+            return 1;
+        }
 
     archiver->archiveData = *archiveData;
 
-    int num_membros = contar_membros(membros);
-
-    // Encaminhar para a função apropriada de acordo com a opção
-    if (strcmp(opcao, "-i") == 0) {
-
-        if (num_membros > 1)
-            inserir_membros_diversos(archiver, membros, num_membros);
-        else{
-            inserir_membros(archiver, argv[3]);
-            printf("%s\n", argv[3]);
+    while (continuar) {
+        // Verificar se foram fornecidos argumentos suficientes
+        if (argc < 3) {
+            printf("Uso: vina++ <opção> <archive> [membro1 membro2 ...]\n");
+            return 1;
         }
 
-    } else if (strcmp(opcao, "-a") == 0) {
-        inserir_data(archiver, membros, num_membros);
-    
-    } else if (strcmp(opcao, "-m") == 0) {
-        /*mover_membro(archive, membros);*/
-        printf("oi\n");
-    } else if (strcmp(opcao, "-x") == 0) {
-        extrair_membros(archiver, membros, num_membros);
-     
-    } else if (strcmp(opcao, "-r") == 0) {
-        remover_membros(archive, membros);
-    } else if (strcmp(opcao, "-c") == 0) {
-        listar_conteudo(archive);
-    } else if (strcmp(opcao, "-h") == 0) {
-        exibir_ajuda();
-    } else {
-        printf("Opção inválida.\n");
-        exibir_ajuda();
+        char *opcao = argv[1];
+        
+        char **membros = argv + 3;  // Pode ser vazio, dependendo da opção
+
+        // Convertendo caminhos absolutos para caminhos relativos
+        char *diretorio_atual = getcwd(NULL, 0);  // Obtém o diretório atual
+        int tamanho_diretorio_atual = strlen(diretorio_atual);
+
+        for (int i = 0; membros[i] != NULL; i++) {
+            if (membros[i][0] != '/') {
+                // O membro não é um caminho absoluto, ignora a conversão
+                continue;
+            }
+
+            char *caminho_relativo = malloc(strlen(membros[i]) + 2 + tamanho_diretorio_atual);
+            if (caminho_relativo == NULL) {
+                printf("Erro ao alocar memória.\n");
+                return 1;
+            }
+
+            strcpy(caminho_relativo, ".");
+            strcat(caminho_relativo, membros[i]);
+
+            membros[i] = caminho_relativo;
+        }
+
+        free(diretorio_atual);
+
+        int num_membros = contar_membros(membros);
+
+        // Encaminhar para a função apropriada de acordo com a opção
+        if (strcmp(opcao, "-i") == 0) {
+
+            if (num_membros > 1)
+                inserir_membros_diversos(archiver, membros, num_membros);
+            else {
+                inserir_membros(archiver, argv[3]);
+                printf("%s\n", argv[3]);
+            }
+
+        } else if (strcmp(opcao, "-a") == 0) {
+            inserir_data(archiver, membros, num_membros);
+
+        } else if (strcmp(opcao, "-m") == 0) {
+            /*mover_membro(archive, membros);*/
+            printf("oi\n");
+
+        } else if (strcmp(opcao, "-x") == 0) {
+            extrair_membros(archiver, membros, num_membros);
+
+        } /*else if (strcmp(opcao, "-r") == 0) {
+            remover_membros(archive, membros);
+
+        } else if (strcmp(opcao, "-c") == 0) {
+            listar_conteudo(archive);
+
+        } else if (strcmp(opcao, "-h") == 0) {
+            exibir_ajuda();
+
+        } else {
+            printf("Opção inválida.\n");
+            exibir_ajuda();
+        }
+        */
+
+        // Solicita novos argumentos
+        printf("Digite os argumentos (ou 0 para sair): ");
+        char linha_argumentos[1000];
+        fgets(linha_argumentos, sizeof(linha_argumentos), stdin);
+        linha_argumentos[strcspn(linha_argumentos, "\n")] = '\0';  // Remove a quebra de linha
+
+        if (strcmp(linha_argumentos, "0") == 0) {
+            continuar = 0;  // Sai do loop e encerra o programa
+        } else {
+            // Atualiza os argumentos para a próxima iteração
+            argc = 0;
+            argv[argc++] = strtok(linha_argumentos, " ");
+
+            char *token;
+            while ((token = strtok(NULL, " ")) != NULL) {
+                argv[argc++] = token;
+            }
+        }
     }
-    
+
+    // Libera a memória alocada
+        free(archiveData);
+        free(archiver);
 
     return 0;
 }
